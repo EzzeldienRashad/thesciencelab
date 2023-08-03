07e867089480b6d973813325dbcc2a8e43814d06
Ezzeldien <ezzeldienrashad@gmail.com>
2023-08-03 16:39:51 +0300

    added password eye & redirect if page not found

--- info/functions/printInfo.php
+++ info/functions/printInfo.php
@@ -9,6 +9,10 @@ if (isset($_SERVER["HTTP_ORIGIN"])) {
 header("Access-Control-Allow-Credentials: true");
 
 $path = "../grades/" . (isset($_GET["grade"]) ? $_GET["grade"] . (isset($_GET["game"]) ? "/" . $_GET["game"] . (isset($_GET["unit"]) && $_GET["unit"] !== "whole" ? "/" . $_GET["unit"] : "") : "") : "");
+if (!file_exists($path)) {
+    echo "[]";
+    exit;
+}
 if (isset($_GET["grade"]) && isset($_GET["game"]) && isset($_GET["unit"])) {
     if ($_GET["unit"] == "whole") {
         $questions = [];
